c7a499482f34988519d1167fa29fe4189791a603
byteninjacyber <byteninjacyber@users.noreply.github.com>
2026-05-06 18:42:00 +0800

    fix: use relative paths for chat widget assets

--- .quartz/quartz/components/ChatWidget.tsx
+++ .quartz/quartz/components/ChatWidget.tsx
@@ -1,11 +1,14 @@
-import { QuartzComponent, QuartzComponentConstructor } from "./types"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { pathToRoot } from "../util/path"
+import { joinSegments } from "../util/path"
 
 export default (() => {
-  const ChatWidget: QuartzComponent = () => {
+  const ChatWidget: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
+    const baseDir = pathToRoot(fileData.slug!)
     return (
       <>
-        <link rel="stylesheet" href="/static/chat.css" />
-        <script src="/static/chat.js" defer></script>
+        <link rel="stylesheet" href={joinSegments(baseDir, "static", "chat.css")} />
+        <script src={joinSegments(baseDir, "static", "chat.js")} defer></script>
       </>
     )
   }
